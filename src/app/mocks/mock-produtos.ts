b48ca86fa66a364655b72b27a5967bf7345d0d82
Leonardo Aoki <leonardoyaoki@gmail.com>
2021-11-19 08:55:24 -0300

    Classe e mock de produtos adicionados

---
+++ src/app/mocks/mock-produtos.ts
@@ -0,0 +1,9 @@
+import { Produto } from "../models/produto";
+
+export const PRODUTOS: Produto[] = [
+  { id: 1, nome: 'Pão', preco: 2.00 },
+  { id: 2, nome: 'Arroz', preco: 20.00 },
+  { id: 3, nome: 'Feijão', preco: 15.00 },
+  { id: 4, nome: 'Macarrão', preco: 10.00 },
+  { id: 5, nome: 'Suco', preco: 5.00 }
+]
