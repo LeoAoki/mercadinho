8c0d27f2e4ac20df6986b6180932bc9ec8e041b7
Leonardo Aoki <leonardoyaoki@gmail.com>
2021-11-19 09:34:39 -0300

    Pipes e valores atualizados

--- src/app/mocks/mock-produtos.ts
+++ src/app/mocks/mock-produtos.ts
@@ -1,9 +1,9 @@
 import { Produto } from "../models/produto";
 
 export const PRODUTOS: Produto[] = [
-  { id: 1, nome: 'Pão', preco: 2.00 },
-  { id: 2, nome: 'Arroz', preco: 20.00 },
+  { id: 1, nome: 'Pão', preco: 1.75 },
+  { id: 2, nome: 'Arroz', preco: 21.99 },
   { id: 3, nome: 'Feijão', preco: 15.00 },
-  { id: 4, nome: 'Macarrão', preco: 10.00 },
-  { id: 5, nome: 'Suco', preco: 5.00 }
+  { id: 4, nome: 'Macarrão', preco: 10.50 },
+  { id: 5, nome: 'Suco', preco: 4.99 }
 ]
